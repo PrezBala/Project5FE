a0dc7b3e79677824b91c366fb75023717f6a1adb
PrezBala <prez5aw@live.com>
2023-05-09 16:13:53 +0000

    add useEffect to fetch URL from API project, testing GET function and add token ID to pull through data

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
 
-  const [movies, setMovies] = useState(['Movie 1','Movie 2']);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://8000-prezbala-project5api-nox8rqq7d9l.ws-eu96b.gitpod.io/api/movies/", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Token 1585932fbeb3f2384cd08c6fe9438600f96e51fa'
+        }
+    })
+    .then( resp => resp.json())
+    .then( resp => setMovies(resp))
+  }, [])
 
   return (
     <div className="App">
