ede10740187e020ab7a098d3e609265eab4f3335
PrezBala <prez5aw@live.com>
2023-05-11 14:48:12 +0000

    correct onMouseEnter and Leave function by incorp a usestate

--- movie-rater/src/components/movie-details.js
+++ movie-rater/src/components/movie-details.js
@@ -1,31 +1,38 @@
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-
-
 
 function MovieDetails(props){
 
+const [ highlighted, setHighlighted ] = useState(-1);
+    
     const mov = props.movie;
 
-
     return (
-        <div>
+        <React.Fragment>
             { mov ? (
                 <div>
-                    <h1>{mov && mov.title}</h1>
-                    <p>{mov && mov.description}</p>
+                    <h1>{mov.title}</h1>
+                    <p>{mov.description}</p>
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 0 ? 'orange':'' } />
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 1 ? 'orange':'' } />
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange':'' } />
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange':'' } />
                     <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange':'' } />
                     ({mov.no_of_ratings})
+                    <div className="rate-container">
+                        <h2>Rate it</h2>
+                        { [...Array(5)].map( (e, i) => {
+                        return <FontAwesomeIcon key={i} icon={faStar} className={highlighted > i - 1 ? 'purple':''}
+                                onMouseEnter={() => setHighlighted(i)}
+                                onMouseLeave={() => setHighlighted(-1)}
+                            />
+                        })}
+                    </div>
                 </div>
             ) : null }
-
-        </div> 
-    )
+        </React.Fragment>
+  )
 }
-  
-  export default MovieDetails;
+
+export default MovieDetails;
