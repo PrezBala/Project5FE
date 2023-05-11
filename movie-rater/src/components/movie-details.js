bff441618354fa4b541a17cdaf87191c8d0c5b1e
PrezBala <prez5aw@live.com>
2023-05-11 10:09:25 +0000

    import fontawesome and test icon function within movie details

--- movie-rater/src/components/movie-details.js
+++ movie-rater/src/components/movie-details.js
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 function MovieDetails(props){
@@ -6,7 +8,14 @@ function MovieDetails(props){
 
     return (
         <div>
-            <h1>{props.movie && props.movie.title}</h1>
+            { props.movie ? (
+                <div>
+                    <h1>{props.movie && props.movie.title}</h1>
+                    <p>{props.movie && props.movie.description}</p>
+                    <FontAwesomeIcon icon={faCoffee} />
+                </div>
+            ) : null }
+
         </div> 
     )
 }
