ec71fa4e2419e7e74030d1a3878840c245ac2601
PrezBala <prez5aw@live.com>
2023-05-11 10:20:12 +0000

    add props movie avg rating for each star within movie details and also props movie no of ratings to show total rating count

--- movie-rater/src/components/movie-details.js
+++ movie-rater/src/components/movie-details.js
@@ -13,11 +13,12 @@ function MovieDetails(props){
                 <div>
                     <h1>{props.movie && props.movie.title}</h1>
                     <p>{props.movie && props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
-                    <FontAwesomeIcon icon={faStar} />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 0 ? 'orange':'' } />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 1 ? 'orange':'' } />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 2 ? 'orange':'' } />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 3 ? 'orange':'' } />
+                    <FontAwesomeIcon icon={faStar} className={props.movie.avg_rating > 4 ? 'orange':'' } />
+                    ({props.movie.no_of_ratings})
                 </div>
             ) : null }
 
