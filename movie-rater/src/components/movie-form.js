d75bd3a65bb4a413612520e1d296b519a6903580
PrezBala <prez5aw@live.com>
2023-05-14 15:23:20 +0000

    add updateclicked function for update box within movie-form.js to allow user to update movie selection name etc

--- movie-rater/src/components/movie-form.js
+++ movie-rater/src/components/movie-form.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState}from 'react';
 
 function MovieForm(props) {
+
+    const [ title, setTitle ] = useState(props.movie.title);
+    const [ description, setDescription ] = useState(props.movie.description);
+
+    const updateClicked = () => {
+        console.log('update here');
+      }
+
+
     return (
         <React.Fragment>
             { props.movie ? (
             <div>
                 <label for="title">Title</label><br/>
-                <input id="title" type="text" placeholder="title" /><br/>
+                <input id="title" type="text" placeholder="title" value={title}
+                    onChange={ evt=> setTitle(evt.target.value)}
+                /><br/>
                 <label for="description">Description</label><br/>
-                <textarea id="description"type="text" placeholder="Description"></textarea><br/>
+                <textarea id="description"type="text" placeholder="Description" value={description}
+                    onChange={ evt=> setDescription(evt.target.value)}
+                ></textarea><br/>
+                <button onClick={updateClicked}>Update</button>
             </div>
             ) : null }
         </React.Fragment>
