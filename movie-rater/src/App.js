817ab6754786c659ed6c5e01f6f083a3bf75e293
PrezBala <prez5aw@live.com>
2023-05-14 14:39:59 +0000

    create movie-form.js and add faEdit and faTrash within movie-list.js

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import MovieList from './components/movie-list';
 import MovieDetails from './components/movie-details';
+import MovieForm from './components/movie-form';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [editedMovie, setEditedMovie] = useState(null);
 
   useEffect(()=>{
     fetch("https://8000-prezbala-project5api-nox8rqq7d9l.ws-eu97.gitpod.io/api/movies/", {
@@ -31,14 +33,20 @@ function App() {
 
   }
 
+  const editClicked = movie => {
+    setEditedMovie(movie);
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <MovieList movies={movies} movieClicked={movieClicked}/>
+        <MovieList movies={movies} movieClicked={movieClicked} editClicked={editClicked}/>
         <MovieDetails movie={selectedMovie} updateMovie={loadMovie}/>
+        <MovieForm movie ={editedMovie} />
       </div>
     </div>
   );
