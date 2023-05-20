b924d23ecad766a2231c4d9d4839454d4354e9fc
PrezBala <>
2023-05-20 20:20:29 +0000

    amended urls in api-service which resolved errors

---
+++ src/api-service.js
@@ -0,0 +1,45 @@
+const TOKEN = "1585932fbeb3f2384cd08c6fe9438600f96e51fa";
+
+export class API {
+  static loginUser(body) {
+    return fetch(`https://8000-prezbala-project5api-nzyoyjptc3.us2.codeanyapp.com/auth/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body)
+    }).then(resp => resp.json());
+  }
+
+  static updateMovie(mov_id, body) {
+    return fetch(`https://8000-prezbala-project5api-nzyoyjptc3.us2.codeanyapp.com/api/movies/${mov_id}/`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${TOKEN}`
+      },
+      body: JSON.stringify(body)
+    }).then(resp => resp.json());
+  }
+
+  static createMovie(body) {
+    return fetch(`https://8000-prezbala-project5api-nzyoyjptc3.us2.codeanyapp.com/api/movies/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${TOKEN}`
+      },
+      body: JSON.stringify(body)
+    }).then(resp => resp.json());
+  }
+
+  static deleteMovie(mov_id) {
+    return fetch(`https://8000-prezbala-project5api-nzyoyjptc3.us2.codeanyapp.com/api/movies/${mov_id}/`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${TOKEN}`
+      }
+    });
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Updated import statement
+import './index.css';
+import App from './App';
+import Auth from './components/auth';
+import reportWebVitals from './reportWebVitals';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+
+function Router() {
+  return (
+    <React.StrictMode>
+      <CookiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/movies" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </CookiesProvider>
+    </React.StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')).render(<Router />); // Updated rendering using createRoot
+
+reportWebVitals();
