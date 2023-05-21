2488acfc7809c307c1e8602cbdb8b6240b61a5a4
PrezBala <>
2023-05-21 14:19:50 +0000

    create new login container within app.cs and created new div classes

--- src/components/auth.js
+++ src/components/auth.js
@@ -28,25 +28,28 @@ function Auth(){
       }
 
     return (
-        <div>
-            
+        <div className="App">
+        <header className="App-header">
             {isLoginView ? <h1>Login</h1> : <h1>Register</h1>}
-            <label htmlFor="username">Username</label><br/>
-            <input id="username" type="text" placeholder="username" value={username}
-                onChange={ evt=> setUsername(evt.target.value)}
-            /><br/>
-            <label htmlFor="password">Password</label><br/>
-            <input id="password"type="text" placeholder="password" value={password}
-                onChange={ evt=> setPassword (evt.target.value)}  /><br/>
-                { isLoginView ?
-                    <button onClick={loginClicked}>Login</button> : 
-                    <button onClick={registerClicked}>Register</button>
-                }
+        </header>
+            <div className="login-container">
+                <label htmlFor="username">Username</label><br/>
+                <input id="username" type="text" placeholder="username" value={username}
+                    onChange={ evt=> setUsername(evt.target.value)}
+                /><br/>
+                <label htmlFor="password">Password</label><br/>
+                <input id="password"type="text" placeholder="password" value={password}
+                    onChange={ evt=> setPassword (evt.target.value)}  /><br/>
+                    { isLoginView ?
+                        <button onClick={loginClicked}>Login</button> : 
+                        <button onClick={registerClicked}>Register</button>
+                    }
 
-            { isLoginView ?
-                <p onClick={()=> setIsLoginView(false)}>You don't have an account? Register here!</p> : 
-                <p onClick={()=> setIsLoginView(true)}>You already have an account? Login here</p>
-            }
+                { isLoginView ?
+                    <p onClick={()=> setIsLoginView(false)}>You don't have an account? Register here!</p> : 
+                    <p onClick={()=> setIsLoginView(true)}>You already have an account? Login here</p>
+                }
+            </div>
         </div>
     )
 }
