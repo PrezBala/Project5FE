1cc549df4eb8432a00cc4025dd2f8160a7455f19
PrezBala <>
2023-05-27 21:02:05 +0000

    import react carosel for images on main page and amend app.js to suit

--- src/App.js
+++ src/App.js
@@ -8,63 +8,15 @@ import { faFilm } from '@fortawesome/free-solid-svg-icons';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { useCookies } from 'react-cookie';
 import { useFetch } from './hooks/useFetch';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+import Movie1 from '/workspaces/Project5FE/public/images/moviereel.jpeg';
+import Movie2 from '/workspaces/Project5FE/public/images/moviereel.jpeg';
+import Movie3 from '/workspaces/Project5FE/public/images/moviereel.jpeg';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-  const [editedMovie, setEditedMovie] = useState(null);
-  const [token, setToken, deleteToken] = useCookies(['mr-token']);
-  const [data, loading, error] = useFetch();
-
-  useEffect(() => {
-    setMovies(data);
-  }, [data]);
-
-  useEffect(() => {
-    if (!token['mr-token']) window.location.href = '/';
-  }, [token]);
-
-  const loadMovie = movie => {
-    setSelectedMovie(movie);
-    setEditedMovie(null);
-  };
-
-  const editClicked = movie => {
-    setEditedMovie(movie);
-    setSelectedMovie(null);
-  };
-
-  const updatedMovie = movie => {
-    const newMovies = movies.map(mov => (mov.id === movie.id ? movie : mov));
-    setMovies(newMovies);
-  };
-
-  const newMovie = () => {
-    setEditedMovie({ title: '', description: '' });
-    setSelectedMovie(null);
-  };
-
-  const movieCreated = movie => {
-    const newMovies = [...movies, movie];
-    setMovies(newMovies);
-  };
-
-  const removeClicked = movie => {
-    const newMovies = movies.filter(mov => mov.id !== movie.id);
-    setMovies(newMovies);
-  };
-
-  const logoutUser = () => {
-    deleteToken(['mr-token']);
-  };
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>Error loading movies</h1>;
-
-  if (movies['detail'] === 'Invalid token.') {
-    logoutUser();
-    return <h1>Wrong credentials, please refresh and try again</h1>;
-  }
+  // your code here...
 
   return (
     <div className="App">
@@ -78,6 +30,22 @@ function App() {
           <span>Log out</span> 
         </div>
       </header>
+
+      <Carousel autoPlay infiniteLoop useKeyboardArrows dynamicHeight>
+        <div>
+            <img src={Movie1} alt="Movie 1" />
+            <p className="legend">Legend 1</p>
+        </div>
+        <div>
+            <img src={Movie2} alt="Movie 2" />
+            <p className="legend">Legend 2</p>
+        </div>
+        <div>
+            <img src={Movie3} alt="Movie 3" />
+            <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+
       <div className="layout">
         <div>
           <MovieList
