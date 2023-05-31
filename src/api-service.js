f126f85373885ca0f630af2870c3d3716802aad9
PrezBala <>
2023-05-31 19:47:51 +0000

    API service code with all methods converted to async functions using the async/await syntax

--- src/api-service.js
+++ src/api-service.js
@@ -1,79 +1,84 @@
 export class API {
-
-    static getUsers(token) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/users/`, {
+    static async getUsers(token) {
+      const response = await fetch(`https://project-5-movierater.herokuapp.com/api/users/`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Token ${token}`
         }
-      }).then(resp => resp.json());
+      });
+      return response.json();
     }
   
-    static deleteUser(user_id, token) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/users/${user_id}/`, {
+    static async deleteUser(user_id, token) {
+      await fetch(`https://project-5-movierater.herokuapp.com/api/users/${user_id}/`, {
         method: 'DELETE',
         headers: {
-           'Content-Type': 'application/json',
-           'Authorization': `Token ${token}`
-          }
+          'Content-Type': 'application/json',
+          'Authorization': `Token ${token}`
+        }
       });
     }  
   
-    static loginUser(body) {
-      return fetch(`https://project-5-movierater.herokuapp.com/auth/`, {
+    static async loginUser(body) {
+      const response = await fetch(`https://project-5-movierater.herokuapp.com/auth/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(body)
-      }).then(resp => resp.json());
+      });
+      return response.json();
     }
   
-    static registerUser(body) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/users/`, {
+    static async registerUser(body) {
+      const response = await fetch(`https://project-5-movierater.herokuapp.com/api/users/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify( body )
-      }).then( resp => resp.json())
+        body: JSON.stringify(body)
+      });
+      return response.json();
     }
   
-    static getMovies(token){
-      return fetch("https://project-5-movierater.herokuapp.com/api/movies/", {
+    static async getMovies(token) {
+      const response = await fetch("https://project-5-movierater.herokuapp.com/api/movies/", {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Token ${token}` 
         }
-      }).then( resp => resp.json())
+      });
+      return response.json();
     }
     
-    static updateMovie(mov_id, body, token) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/movies/${mov_id}/`, {
+    static async updateMovie(mov_id, body, token) {
+      const response = await fetch(`https://project-5-movierater.herokuapp.com/api/movies/${mov_id}/`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Token ${token}`
         },
         body: JSON.stringify(body)
-      }).then(resp => resp.json());
+      });
+      return response.json();
     }
   
-    static createMovie(body, token) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/movies/`, {
+    static async createMovie(body, token) {
+      const response = await fetch(`https://project-5-movierater.herokuapp.com/api/movies/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Token ${token}`
         },
         body: JSON.stringify(body)
-      }).then(resp => resp.json());
+      });
+      return response.json();
     }
   
-    static deleteMovie(mov_id, token) {
-      return fetch(`https://project-5-movierater.herokuapp.com/api/movies/${mov_id}/`, {
+    static async deleteMovie(mov_id, token) {
+      await fetch(`https://project-5-movierater.herokuapp.com/api/movies/${mov_id}/`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
