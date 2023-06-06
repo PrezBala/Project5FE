0d22dee63ea372dd6d66d1d56be43ded5aa60b43
PrezBala <>
2023-06-06 08:26:36 +0000

    amend color and styling within admin panel to wine

--- src/components/adminpanel.js
+++ src/components/adminpanel.js
@@ -6,26 +6,12 @@ import './adminpanel.css';
 
 function AdminPanel() {
   const [token] = useCookies(['mr-token']);
-  const [usersData, setUsersData] = useState([]);
   const [moviesData, setMoviesData] = useState([]);
-  const [usersLoading, setUsersLoading] = useState(true);
   const [moviesLoading, setMoviesLoading] = useState(true);
-  const [usersError, setUsersError] = useState(null);
   const [moviesError, setMoviesError] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const users = await API.getUsers(token['mr-token']);
-        setUsersData(users);
-        setUsersLoading(false);
-      } catch (error) {
-        setUsersError(error.message);
-        setUsersLoading(false);
-      }
-    }
-
     async function fetchMovies() {
       try {
         const movies = await API.getMovies(token['mr-token']);
@@ -37,14 +23,8 @@ function AdminPanel() {
       }
     }
 
-    fetchUsers();
     fetchMovies();
   }, [token]);
-
-  const deleteUser = async (userId) => {
-    await API.deleteUser(userId, token['mr-token']);
-    window.location.reload();
-  }
 
   const deleteMovie = async (movieId) => {
     await API.deleteMovie(movieId, token['mr-token']);
@@ -55,24 +35,13 @@ function AdminPanel() {
     navigate(-1);
   }
 
-  if (usersLoading || moviesLoading) return <h1>Loading...</h1>;
-  if (usersError) return <h1>Error loading users: {usersError}</h1>;
+  if (moviesLoading) return <h1>Loading...</h1>;
   if (moviesError) return <h1>Error loading movies: {moviesError}</h1>;
 
   return (
     <div className="admin-panel">
       <h1>Admin Panel</h1>
       <button onClick={goBack}>Back</button>
-
-      <section className="section">
-        <h2>Users</h2>
-        {usersData.map((user) => (
-          <div key={user.id} className="item">
-            <p className="item-detail">{user.username}</p>
-            <button className="item-button" onClick={() => deleteUser(user.id)}>Delete {user.username}</button>
-          </div>
-        ))}
-      </section>
 
       <section className="section">
         <h2>Movies</h2>
